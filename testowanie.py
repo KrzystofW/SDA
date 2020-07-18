4faa676bb772d3e0bbe5750ca196a3bfa9fd00a1
Krzysztof Wajchert <krzysiek@kurs.pl>
2020-07-18 13:02:12 +0200

    Initial commit

---
+++ testowanie.py
@@ -0,0 +1,12 @@
+cena_hondy = 21000
+remont = 7000
+budzet = 30000
+
+wydatki = cena_hondy + remont
+pozostalo = budzet - wydatki
+
+print(f'Wydałes na auto {wydatki} i zostało Ci {pozostalo}')
+
+import math
+
+print(math.sqrt(2))
